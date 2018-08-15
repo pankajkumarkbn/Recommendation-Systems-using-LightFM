639afa3cd3c9f20528f05e0473e5ea701b80ffb4
pravinvedurla <vpravin98@gmail.com>
2018-08-15 21:25:58 +0530

    Final

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Recommendation-Systems-using-LightFM
 Recommendation system using LightFM library that recommends Movies to Users based on their Rating history.
+
+simply install LightFM using PIP or Conda according to your Env setup.
+
+Then activate your environment and type
+
+```
+python systems.py
+```
+
+Change the user Ids in the last line of Function call according to your preference.
--- systems.py
+++ systems.py
@@ -4,5 +4,44 @@ from lightfm import LightFM
 
 dt = fetch_movielens(min_rating=5.0)
 
-print(repr(dt['train']))
-print(repr(dt['test']))
+print("train - " + repr(dt['train']))
+print("test - " + repr(dt['test']))
+
+model = LightFM(loss='warp')
+#train model
+model.fit(dt['train'], epochs=30, num_threads=2)
+
+
+#Modify this function so that it parses your dataset correctly to retrieve
+#the necessary variables (products, songs, tv shows, etc.)
+#then print out the recommended results 
+
+def sample_recommendation(model, data, user_ids):
+
+    #number of users and movies in training data
+    n_users, n_items = data['train'].shape
+
+    #generate recommendations for each user we input
+    for user_id in user_ids:
+
+        #movies they already like
+        known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+
+        #movies our model predicts they will like
+        scores = model.predict(user_id, np.arange(n_items))
+        #rank them in order of most liked to least
+        top_items = data['item_labels'][np.argsort(-scores)]
+
+        #print out the results
+        print("User %s" % user_id)
+        print("     Known positives:")
+
+        for x in known_positives[:3]:
+            print("        %s" % x)
+
+        print("     Recommended:")
+
+        for x in top_items[:3]:
+            print("        %s" % x)
+            
+sample_recommendation(model, dt, [3, 25, 450])
